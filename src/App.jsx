52aafeb20956e1e34b198d962272a69637a6410a
noisetones-sys <noisetones@gmail.com>
2025-10-31 20:39:04 +0530

    Add initial App component with success message

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', paddingTop: '50px' }}>
+      <h1>It Works 🎉</h1>
+      <p>Your app is live on GitHub Pages!</p>
+    </div>
+  )
+}
